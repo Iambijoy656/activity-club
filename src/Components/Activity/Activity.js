63fc5d23c43065d31fc61c1673ea0dab418b5716
Bijoy Chandro Das <freelancing.bijoy@gmail.com>
2022-09-29 03:05:39 +0600

    fetch add

---
+++ src/Components/Activity/Activity.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Activity = () => {
+    return (
+        <div>
+            <h2>This is Ativity</h2>
+        </div>
+    );
+};
+
+export default Activity;
--- src/Components/Display/Display.js
+++ src/Components/Display/Display.js
@@ -1,13 +1,25 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import Activity from '../Activity/Activity';
 
 const Display = () => {
-    const [activities, setActivities] = useState([])
+    const [activities, setActivities] = useState([]);
+
+    useEffect(() => {
+        fetch('activities.json')
+            .then(res => res.json())
+            .then(data => setActivities(data))
+    }, [])
 
 
     return (
         <div className='grid grid-cols-6 gap-4'>
             <div className='col-start-1 col-end-5'>
-                <h3>Activities</h3>
+                {
+                    activities.map(activity =>
+                        <Activity
+                            key={activity.id}
+                        ></Activity>)
+                }
             </div>
 
 
@@ -15,7 +27,7 @@ const Display = () => {
             <div className='text-center'>
                 <h3>user information</h3>
             </div>
-        </div>
+        </div >
     );
 };
 
