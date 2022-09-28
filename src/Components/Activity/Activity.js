31ab9a3e0544f8b3b2ee2feabad649b89e5e8346
Bijoy Chandro Das <freelancing.bijoy@gmail.com>
2022-09-29 03:29:56 +0600

    load data

--- src/Components/Activity/Activity.js
+++ src/Components/Activity/Activity.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
-const Activity = () => {
+const Activity = (props) => {
+    // console.log(props.activity)
+    const { name, picture, time, age } = props.activity;
     return (
         <div>
-            <h2>This is Ativity</h2>
+            <div className="card card-compact bg-base-100 shadow-xl">
+                <figure><img src={picture} alt="pics" /></figure>
+                <div className="card-body">
+                    <h2 className="card-title">{name}</h2>
+                    <p>Finding motivation to work out can be challenging. There are so many distractions and less physically demanding alternatives to spending an hour at the gym or working out at home</p>
+                    <p><span className='font-bold'>For age:</span> {age} </p>
+                    <p><span className='font-bold'>Time Required :</span> {time}s </p>
+                    <div className="card-actions justify-end">
+                        <button className="btn btn-primary">Buy Now</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Components/Display/Display.js
+++ src/Components/Display/Display.js
@@ -13,11 +13,12 @@ const Display = () => {
 
     return (
         <div className='grid grid-cols-6 gap-4'>
-            <div className='col-start-1 col-end-5'>
+            <div className='col-start-1 col-end-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
                 {
                     activities.map(activity =>
                         <Activity
                             key={activity.id}
+                            activity={activity}
                         ></Activity>)
                 }
             </div>
